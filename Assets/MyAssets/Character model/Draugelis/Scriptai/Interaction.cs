2cc5a8d01c8063f6bdad4e6b71df8bbcc33e3176
Eds48 <edasm05@gmail.com>
2026-03-20 16:04:58 +0200

    Added a script to be able to interact with the NPC

---
+++ Assets/MyAssets/Character model/Draugelis/Scriptai/Interaction.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    bool player_detection = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if(player_detection && Input.GetKeyDown(KeyCode.E))
+        {
+            print("Pisam parūkyt");
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Capsule Mesh")
+        player_detection = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        player_detection = false;
+    }
+}
