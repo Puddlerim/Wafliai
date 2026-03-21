2d80e968f79ef5dd917285f5ee88c826e1330a10
Eds48 <edasm05@gmail.com>
2026-03-21 14:23:31 +0200

    Added dialogue and dialogue manager scripts

---
+++ Assets/MyAssets/Scriptai/DialogueManager.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Text;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject dialoguePanel;
+
+    [SerializeField]
+    TextMeshProUGUI dialogueText;
+
+    [SerializeField]
+    GameObject buttonPrefab;
+
+    [SerializeField]
+    Transform buttonsParent;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void BeginDialogue(Dialogue dialogue)
+    {
+        if (dialogue.Choices.Count == 0)
+        {
+            dialoguePanel.SetActive(false);
+
+            FirstPersonMovement.ToggleMovement(false);
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        FirstPersonMovement.ToggleMovement(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        dialoguePanel.SetActive(true);
+
+        ClearChoices();
+        AnimateText(dialogue);
+    }
+
+    void ClearChoices()
+    {
+        foreach (Transform child in buttonsParent)
+            Destroy(child.gameObject);
+    }
+    void AnimateText(Dialogue dialogue)
+    {
+        IEnumerator TypeText(string text)
+        {
+            StringBuilder textToShow = new StringBuilder();
+            for (int i = 0; i < text.Length; i++)
+            {
+                textToShow.Append(text[i]);
+                dialogueText.text = textToShow.ToString();
+
+                yield return new WaitForSeconds(1f / 20f);
+            }
+            ShowChoices(dialogue);
+        }
+        StartCoroutine(TypeText(dialogue.DialogueText));
+    }
+    void ShowChoices(Dialogue dialogue)
+    {
+        foreach (Dialogue choice in dialogue.Choices)
+        {
+            GameObject newButton = Instantiate(buttonPrefab, buttonsParent);
+
+            newButton.GetComponentInChildren<TextMeshProUGUI>().text = choice.OptionName;
+            newButton.GetComponent<Button>().onClick.AddListener(() =>
+            {
+                BeginDialogue(choice);
+            });
+        }
+    }
+}
